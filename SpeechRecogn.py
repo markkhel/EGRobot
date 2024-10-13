3759c2a615a77b3bdacde3b7112521a5cdd645bf
Ryan <ryanchan92305@gmail.com>
2024-10-13 23:07:26 +0000

    Ryan's code for speechRecogntion

--- SpeechRecogn.py
+++ SpeechRecogn.py
@@ -1,1 +1,15 @@
 import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print ("say sum")
+    audio = recognizer.listen(source)
+    
+try:
+    command = recognizer.recognize_sphinx(audio)
+    print ('you said:' + command)
+except sr.UnknownValueError:
+    print ('cant understand')
+except sr.RequestError as e:
+    print("error" + e)
