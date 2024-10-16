0b3127f836721d30c56053dd075a841efebda942
Tim H <halley.tim.227@gmail.com>
2024-10-16 00:06:27 -0400

    Edit MotorTest.py to add movement for 2nd motor and test HZ and duty cycle because its too fast

--- MotorTest.py
+++ MotorTest.py
@@ -8,14 +8,22 @@ GPIO.setmode(GPIO.BOARD)
 in1 = 18
 in2 = 16
 en = 22
+in3 = 15
+in4 = 7
+enb = 11
 temp1=1
 
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+p=GPIO.PWM(en,180)
 
 p.start(22)
 print("\n")
@@ -32,11 +40,15 @@ while(1):
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3, GPIO.HIGH)
+         GPIO.output(in4, GPIO.LOW)
          print("forward")
          x='z'
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3, GPIO.LOW)
+         GPIO.output(in4, GPIO.HIGH)
          print("backward")
          x='z'
 
@@ -45,12 +57,16 @@ while(1):
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.LOW)
         x='z'
 
     elif x=='f':
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3, GPIO.HIGH)
+        GPIO.output(in4, GPIO.LOW)
         temp1=1
         x='z'
 
@@ -58,6 +74,8 @@ while(1):
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3, GPIO.LOW)
+        GPIO.output(in4, GPIO.HIGH)
         temp1=0
         x='z'
 
