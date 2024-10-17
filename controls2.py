f6290d1210f8bb7dfd0aa7ad247025bf2cdffd58
Timothy <halley.tim.227@gmail.com>
2024-10-17 04:19:51 -0400

    Controller triangle button now can move motors forward upon press and hold and stop motors moving upon release of triangle button

--- controls2.py
+++ controls2.py
@@ -1,6 +1,34 @@
+import RPi.GPIO as GPIO          
+from time import sleep
 from pyPS4Controller.controller import Controller
-    
-    
+GPIO.setmode(GPIO.BOARD)
+
+in1 = 18
+in2 = 16
+en = 22
+in3 = 15
+in4 = 7
+enb = 11
+temp1=1
+
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+p=GPIO.PWM(en,4)
+p2=GPIO.PWM(enb,4)
+
+p.start(30)
+p2.start(30)
+
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+
 class MyController(Controller):
 
     def __init__(self, **kwargs):
@@ -51,9 +79,21 @@ class MyController(Controller):
     def on_L3_x_at_rest(self):
        pass 
     def on_L3_y_at_rest(self):
-       pass 
+       pass
+    def on_triangle_press(self):
+       p.ChangeDutyCycle(40)
+       p2.ChangeDutyCycle(40)
+       GPIO.output(in1,GPIO.HIGH)
+       GPIO.output(in2,GPIO.LOW)
+       GPIO.output(in3, GPIO.HIGH)
+       GPIO.output(in4, GPIO.LOW)
 
-
+    def on_triangle_release(self):
+       GPIO.output(in1,GPIO.LOW)
+       GPIO.output(in2,GPIO.LOW)
+       GPIO.output(in3, GPIO.LOW)
+       GPIO.output(in4, GPIO.LOW)
+       
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
 controller.listen(timeout=60)
