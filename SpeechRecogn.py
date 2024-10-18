6083a2ac3ca45207ccd26b1f7ee51e453265031a
omirthewiz <Omirhenriquez@gmail.com>
2024-10-17 22:54:40 -0400

    Update SpeechRecogn.py

--- SpeechRecogn.py
+++ SpeechRecogn.py
@@ -1,1 +1,21 @@
 import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something...")
+    
+    recognizer.adjust_for_ambient_noise(source)
+    audio = recognizer.listen(source)
+    
+try:
+    command = recognizer.recognize_sphinx(audio)
+    print('You said: ' + command)
+    
+except sr.UnknownValueError:
+    print("Could not understand the audio")
+    
+except sr.RequestError as e:
+    print(f"Request error occurred: {e}")
+f
+
